878a90c5d0dc71fbbab72cde15a66a1abf27bed7
gitaellison <gitaellison@gmail.com>
2016-11-13 21:50:48 -0500

    output added

--- src/main/scala/edu/towson/cosc/cosc455/gellison/project1/MySemanticAnalyzer.scala
+++ src/main/scala/edu/towson/cosc/cosc455/gellison/project1/MySemanticAnalyzer.scala
@@ -4,6 +4,8 @@
   */
 package edu.towson.cosc.cosc455.gellison.project1
 
+import java.awt.Desktop
+import java.io._
 
 class MySemanticAnalyzer {
   var converTree = new scala.collection.mutable.Stack[String]
@@ -131,4 +133,30 @@ class MySemanticAnalyzer {
 
   }
 
+  val file = new File("output.html")
+  print()
+  openHTMLFileInBrowser("output.html")
+
+
+  /* * Hack Scala/Java function to take a String filename and open in default web browswer. */
+  def openHTMLFileInBrowser(htmlFileStr : String) = {
+    val file : File = new File(htmlFileStr.trim)
+    println(file.getAbsolutePath)
+    if (!file.exists())
+      sys.error("File " + htmlFileStr + " does not exist.")
+
+    try {
+      Desktop.getDesktop.browse(file.toURI)
+    }
+    catch {
+      case ioe: IOException => sys.error("Failed to open file:  " + htmlFileStr)
+      case e: Exception => sys.error("He's dead, Jim!")
+    }
+  }
+
+  def print(): Unit ={
+    val pw = new PrintWriter(file))
+    pw.write(converTree.reverse.toString())
+    pw.close
+  }
 }
